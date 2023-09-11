a5c48bf667745d6cfd1d235ab50f99541949dae6
gabriel barbato <gabrielbsp12@gmail.com>
2023-09-11 18:08:33 -0300

    More changes

--- HostName_Attribution_Sheets.ps1
+++ HostName_Attribution_Sheets.ps1
@@ -1,8 +1,8 @@
 # URL da planilha no Google Sheets (substitua pelo URL da sua planilha)
 $planilhaURL = 'https://docs.google.com/spreadsheets/d/ID_DA_PLANILHA/export?format=csv'
 
-# Caminho para onde salvar o arquivo CSV temporário
-$csvFilePath = 'C:\Windows\Temp\'
+# Defina o caminho do arquivo CSV em um diretório público
+$csvFilePath = 'C:\Users\planilha.csv'
 
 # Baixa a planilha como um arquivo CSV
 Invoke-WebRequest -Uri $planilhaURL -OutFile $csvFilePath
@@ -16,22 +16,10 @@ $Serial = (Get-WmiObject -Class Win32_BIOS).SerialNumber
 # Encontre o hostname correspondente com base no número de série
 $Hostname = $csvData | Where-Object { $_.Serial -eq $Serial } | Select-Object -ExpandProperty Hostname
 
-# Verifique se o hostname foi encontrado
-if ($Hostname -ne $null) {
-    # Define o novo hostname
-    Rename-Computer -NewName $Hostname
-
-    # Verifique se o novo hostname foi definido com sucesso
-    if ((Get-ComputerInfo).CsDNSHostName -eq $Hostname) {
-        Write-Host "O hostname foi definido como $Hostname"
-    }
-    else {
-        Write-Host "Erro ao definir o hostname como $Hostname"
-    }
-}
-else {
-    Write-Host "O número de série não está na planilha"
-}
+# Define o novo hostname
+Rename-Computer -NewName $Hostname
 
 # Remova o arquivo CSV temporário
 Remove-Item -Path $csvFilePath -Force
+
+Write-Host "O computador foi renomeado para: $Hostname"
